f8962bb12b6e716852cbeb9e27a6a0a5f56741e7
Aslam Najeebdeen <aslamnajeem@gmail.com>
2012-02-14 07:50:26 +0530

    Initial Rails skeleton

---
+++ README.md
@@ -0,0 +1,8 @@
+# Cuberary
+
+This is one of my pet project to manage and track my online learnings.
+
+
+## License
+
+Cuberary is Copyright (c) 2011 [Aslam Najeebdeen](http://aslamnajeebdeen.com) ([@aslamnd](http://twitter.com/aslamnd)), and is released under MIT-LICENSE.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cuberary::Application.config.secret_token = '644ffcda8d85ae6d69e4b0293dde779167039e299add08106a0dbdee64c2cc5fdd05c5fbe1e68469f757b38221e79e875dbf40d5665a53f3949b08ad5af19d27'
